7ed313cb7724ff55ce4aab6fbfdda7f31872dbd7
Jonatas <djonatas31@gmail.com>
2025-02-28 09:54:33 -0300

    feat: add Sender service

---
+++ sender-api/src/test/java/com/github/jonatas00/microservices/senderapi/SenderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jonatas00.microservices.senderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenderApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
