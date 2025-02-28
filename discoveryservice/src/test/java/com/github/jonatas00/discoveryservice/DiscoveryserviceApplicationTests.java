de87dc31ea81684efd8e3dcdb75b39b45288ed0c
Jonatas00 <djonatas31@gmail.com>
2025-02-27 22:48:06 -0300

    update all

---
+++ discoveryservice/src/test/java/com/github/jonatas00/discoveryservice/DiscoveryserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jonatas00.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
