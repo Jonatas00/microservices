ab0bf25a12af1b7c7d40f8ceb5135a41472b3b39
Jonatas <djonatas31@gmail.com>
2025-02-28 10:37:19 -0300

    feat: add routes

--- sender-api/src/main/java/com/github/jonatas00/microservices/senderapi/SenderApiApplication.java
+++ sender-api/src/main/java/com/github/jonatas00/microservices/senderapi/SenderApiApplication.java
@@ -2,14 +2,23 @@ package com.github.jonatas00.microservices.senderapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class SenderApiApplication {
-
   public static void main(String[] args) {
     SpringApplication.run(SenderApiApplication.class, args);
   }
-
 }
+
+@RestController
+@RequestMapping("sender")
+class Sendercontroller {
+  @GetMapping
+  public String sender() {
+    return "Sender message";
+  }
+}
+
